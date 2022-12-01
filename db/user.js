bda91889d1e2bfe33855436f977be7cc716d7c2b
Akshay Sanjay Bodkhe <abodkhe011@gmail.com>
2022-12-01 15:44:06 +0530

    save address and password

--- db/user.js
+++ db/user.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const userSechma = new mongoose.Schema({
     name: String,
     email: String,
-    contact: String
+    contact: String,
+    address: String,
+    password: String
 })
 
 module.exports = mongoose.model("users", userSechma);
